5588d7312d5d0b252834c2680b5871dc3594a668
AthenaAI <ikram.ali@athenasol.ai>
2024-03-12 15:14:31 +0500

    changes reverted

--- tracker/utils.py
+++ tracker/utils.py
@@ -59,7 +59,7 @@ def get_gps(ser_gps):
 
 async def process_frames(preview_queue, tracklets_queue):
     start_time = time.time()
-    interval = 10
+    interval = 300
     img_frame  = None
     objects_track_history = {}
     # Open serial connection for continuous GPS data reading
